bcbcd56138292c7eb66f99ebb63536b1d543e852
Mateusz Bochynski <matbochynski@gmail.com>
2020-02-22 22:06:01 +0100

    Follow the mouse and rotate

--- mouseCanvas.js
+++ mouseCanvas.js
@@ -21,29 +21,53 @@ function generateRandomColor() {
 }
 
 var tracePoints = [];
+var ball;
 window.onload = function myfunction() {
   canvas = document.getElementById("mouse-path");
   canvasContext = canvas.getContext("2d");
 
   resize();
 
-  this.canvasContext.save();
+  ball = new Ball();
 
-  this.canvasContext.translate(75, 75);
-
-  const slices = 8;
-  for (let i = 0; i < slices; ++i) {
-    this.canvasContext.rotate((Math.PI * 2) / slices);
-    canvasContext.beginPath();
-    canvasContext.moveTo(0, 0);
-    this.canvasContext.lineTo(0 + 50, 0);
-    canvasContext.arc(0, 0, 50, 0, (Math.PI * 2) / slices, false);
-    this.canvasContext.fillStyle = this.generateRandomColor();
-    canvasContext.fill();
-  }
-
-  this.canvasContext.restore();
+  animate();
 };
+
+function Ball() {
+  this.x = mousePosition.x;
+  this.y = mousePosition.y;
+  this.slices = 12;
+
+  this.colors = Array.from(Array(this.slices), () => generateRandomColor());
+
+  console.log(this.colors);
+  this.theta = Math.random() * Math.PI * 2;
+
+  Ball.prototype.draw = function() {
+    this.x = mousePosition.x;
+    this.y = mousePosition.y;
+
+    this.theta += 0.02; // TODO move ot const - this is "spin coevicient"
+
+    canvasContext.save();
+
+    canvasContext.translate(this.x, this.y);
+    canvasContext.rotate(this.theta);
+
+    const slices = this.slices;
+    for (let i = 0; i < slices; ++i) {
+      canvasContext.rotate((Math.PI * 2) / slices);
+      canvasContext.beginPath();
+      canvasContext.moveTo(0, 0);
+      canvasContext.lineTo(0 + 50, 0);
+      canvasContext.arc(0, 0, 50, 0, (Math.PI * 2) / slices, false);
+      canvasContext.fillStyle = this.colors[i];
+      canvasContext.fill();
+    }
+
+    canvasContext.restore();
+  };
+}
 
 window.onresize = function() {
   resize();
@@ -56,39 +80,9 @@ function resize() {
   // TODO recalculate points position or update them to match mouse position?
 }
 
-function TracePoint(x, y) {
-  this.x = x;
-  this.y = y;
-  this.color = generateRandomColor();
-  this.theta = Math.random() * Math.PI * 2;
-  this.radius = Math.random() * 150;
-}
-
-TracePoint.prototype.draw = function() {
-  const previousPosition = {
-    x: this.x,
-    y: this.y
-  };
-
-  this.theta += 0.02; // TODO move ot const - this is "spin coevicient"
-
-  // calculate new position
-  this.x = mousePosition.x + Math.cos(this.theta) * this.radius;
-  this.y = mousePosition.y + Math.sin(this.theta) * this.radius;
-  canvasContext.beginPath();
-  canvasContext.lineWidth = 4; // TODO move to const
-  canvasContext.strokeStyle = this.color;
-  canvasContext.moveTo(previousPosition.x, previousPosition.y);
-  canvasContext.lineTo(this.x, this.y);
-  canvasContext.stroke();
-  canvasContext.closePath();
-};
-
 function animate() {
   requestAnimationFrame(animate);
-  canvasContext.fillStyle = "rgba(0,0,0,0.05)";
+  canvasContext.fillStyle = "rgba(255,255,255,0.25)";
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-  tracePoints.forEach(function(tracePoint, i) {
-    tracePoint.draw();
-  });
+  ball.draw();
 }
